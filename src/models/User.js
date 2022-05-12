2ba2864761010453b318aec7c60031e85f0b3ec8
SIAF AC Rosales Julieta Agostina <jarosales@ara.mil.ar>
2022-05-12 12:35:47 -0300

    ocho commit

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+// 1. guardar al usuario en la db
+//2.buscar al usuario que se quiere loguear por su emaail
+//3. editar la informacion de un usuario
+//4.eliminar a un usuario en la db
+
+const user = {
+    fileName : "./datebase/user.json",
+    create: function(userData){
+
+    }
+
+}
